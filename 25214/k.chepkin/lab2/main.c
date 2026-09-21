28adb35960aa50bd3a8f00f66334037811ebeb0b
Kirill Chepkin <k.chepkin@g.nsu.ru>
2026-09-21 09:59:58 +0700

    rewrote program without the use of export command

--- 25214/k.chepkin/lab2/main.c
+++ 25214/k.chepkin/lab2/main.c
@@ -13,19 +13,23 @@ int main() {
     int pret;
     ret = time(&now);
     if (ret == -1) {
-        exit(0);
+        exit(1);
+    }
+    pret = setenv("TZ", "PST8PDT", 1);
+    if (pret == -1) {
+        exit(1);
     }
     sp = localtime(&now);
     pret = printf("%s", ctime( &now ) );
     if (pret == -1) {
-        exit(0);
+        exit(1);
     }
     pret = printf("%d/%d/%02d %d:%02d %s\n",
                                     sp -> tm_mon + 1, sp -> tm_mday,
                                     sp -> tm_year + 1900, sp -> tm_hour,
                                     sp -> tm_min, tzname[sp -> tm_isdst]);
     if (pret == -1) {
-        exit(0);
+        exit(1);
     }
     exit(0);
 }
--- 25214/k.chepkin/lab2/script.sh
+++ 25214/k.chepkin/lab2/script.sh
@@ -1,6 +1,3 @@
-export TZ="PST8PDT"
 gcc main.c -o main
-./main
-export TZ="GMT-7"
 ./main
 rm main
